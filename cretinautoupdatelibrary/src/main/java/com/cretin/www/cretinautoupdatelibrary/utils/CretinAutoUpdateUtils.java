6f588c18735d2ce18e19c4438c6cdcb731461752
MZCretin <mxnzp_life@163.com>
2017-03-22 09:54:52 +0800

    fix bug

--- cretinautoupdatelibrary/src/main/java/com/cretin/www/cretinautoupdatelibrary/utils/CretinAutoUpdateUtils.java
+++ cretinautoupdatelibrary/src/main/java/com/cretin/www/cretinautoupdatelibrary/utils/CretinAutoUpdateUtils.java
@@ -445,7 +445,7 @@ public class CretinAutoUpdateUtils {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(context, "com.cretin.www.cretinautoupdatelibrary.fileprovider", file);
+            Uri contentUri = FileProvider.getUriForFile(context, context.getPackageName()+".fileprovider", file);
             intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
         } else {
             intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
