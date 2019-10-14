f581dfcf9c2e43b1abbd4544f39719f2623d4083
雷神 <mxnzp_life@163.com>
2019-10-14 18:54:36 +0800

    更新文档

--- app/src/main/java/com/cretin/www/autoupdateproject/app/BaseApp.java
+++ app/src/main/java/com/cretin/www/autoupdateproject/app/BaseApp.java
@@ -20,14 +20,14 @@ public class BaseApp extends Application {
         //更新库配置
         UpdateConfig updateConfig = new UpdateConfig()
                 .setDebug(true)//是否是Debug模式
-                .setBaseUrl("http://www.cretinzp.com/system/versioninfo")
-                .setMethodType(TypeConfig.METHOD_GET)
-                .setDataSourceType(TypeConfig.DATA_SOURCE_TYPE_JSON)
-                .setShowNotification(true)
-                .setNotificationIconRes(R.mipmap.download_icon)
-                .setUiThemeType(TypeConfig.UI_THEME_AUTO)
-                .setRequestHeaders(null)
-                .setRequestParams(null)
+                .setBaseUrl("http://www.cretinzp.com/system/versioninfo")//当dataSourceType为DATA_SOURCE_TYPE_URL时，配置此接口用于获取更新信息
+                .setMethodType(TypeConfig.METHOD_GET)//当dataSourceType为DATA_SOURCE_TYPE_URL时，设置请求的方法
+                .setDataSourceType(TypeConfig.DATA_SOURCE_TYPE_URL)//设置获取更新信息的方式
+                .setShowNotification(true)//配置更新的过程中是否在通知栏显示进度
+                .setNotificationIconRes(R.mipmap.download_icon)//配置通知栏显示的图标
+                .setUiThemeType(TypeConfig.UI_THEME_AUTO)//配置UI的样式，一种有12种样式可供选择
+                .setRequestHeaders(null)//当dataSourceType为DATA_SOURCE_TYPE_URL时，设置请求的请求头
+                .setRequestParams(null)//当dataSourceType为DATA_SOURCE_TYPE_URL时，设置请求的请求参数
                 .setModelClass(new UpdateModel());
         AppUpdateUtils.init(this, updateConfig);
     }
