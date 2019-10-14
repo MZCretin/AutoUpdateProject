1e58b920af555b6c1ac46ecb4778d6fea4adb3c7
雷神 <mxnzp_life@163.com>
2019-10-14 18:51:18 +0800

    文档说明

--- app/src/main/java/com/cretin/www/autoupdateproject/app/BaseApp.java
+++ app/src/main/java/com/cretin/www/autoupdateproject/app/BaseApp.java
@@ -2,6 +2,7 @@ package com.cretin.www.autoupdateproject.app;
 
 import android.app.Application;
 
+import com.blankj.utilcode.util.Utils;
 import com.cretin.www.autoupdateproject.R;
 import com.cretin.www.autoupdateproject.model.UpdateModel;
 import com.cretin.www.cretinautoupdatelibrary.model.TypeConfig;
@@ -13,13 +14,20 @@ public class BaseApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        //BlankJ的初始化 跟更新库没有关系哦
+        Utils.init(this);
+
+        //更新库配置
         UpdateConfig updateConfig = new UpdateConfig()
-                .setDebug(true)
+                .setDebug(true)//是否是Debug模式
                 .setBaseUrl("http://www.cretinzp.com/system/versioninfo")
                 .setMethodType(TypeConfig.METHOD_GET)
                 .setDataSourceType(TypeConfig.DATA_SOURCE_TYPE_JSON)
                 .setShowNotification(true)
                 .setNotificationIconRes(R.mipmap.download_icon)
+                .setUiThemeType(TypeConfig.UI_THEME_AUTO)
+                .setRequestHeaders(null)
+                .setRequestParams(null)
                 .setModelClass(new UpdateModel());
         AppUpdateUtils.init(this, updateConfig);
     }
